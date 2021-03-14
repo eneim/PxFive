a6d6cb1653d5c56e9b6e25a1e764153489764715
eneim <1776230+eneim@users.noreply.github.com>
2021-03-14 11:56:57 +0900

    Fix spotless.

--- app/src/main/java/com/example/androiddevchallenge/ui/screen/Login.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/screen/Login.kt
@@ -17,7 +17,6 @@ package com.example.androiddevchallenge.ui.screen
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -34,8 +33,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.PasswordVisualTransformation
